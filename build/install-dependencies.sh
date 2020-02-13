16a05a7a2e2b0a9a22d0c425b8b1f6bd6944ce14
liuwei <liuweixa@cn.ibm.com>
2020-02-13 14:35:01 +0800

    enable lint

--- build/install-dependencies.sh
+++ build/install-dependencies.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
 
-set -o errexi
-set -o nounset
-set -o pipefail
-set -o xtrace
-
-# Install lint tools
-LINT_TOOLS_PATH="${HOME}"/lint-tools
-mkdir -p "${LINT_TOOLS_PATH}"
-
-wget -P "${LINT_TOOLS_PATH}" https://github.com/hadolint/hadolint/releases/download/v1.17.5/hadolint-Linux-x86_64
-mv "${LINT_TOOLS_PATH}"/hadolint-Linux-x86_64 "${LINT_TOOLS_PATH}"/hadolint
-chmod +x "${LINT_TOOLS_PATH}"/hadolint
+exit 0;
---
+++ build/run-code-lint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -o errexi
+set -o nounset
+set -o pipefail
+set -o xtrace
+
+# prepare lint tools
+LINT_TOOLS_PATH="${HOME}"/lint-tools
+
+mkdir -p "${LINT_TOOLS_PATH}"
+
+wget -P "${LINT_TOOLS_PATH}" https://github.com/hadolint/hadolint/releases/download/v1.17.5/hadolint-Linux-x86_64
+mv "${LINT_TOOLS_PATH}"/hadolint-Linux-x86_64 "${LINT_TOOLS_PATH}"/hadolint
+chmod +x "${LINT_TOOLS_PATH}"/hadolint
+
+export PATH="${LINT_TOOLS_PATH}":"${PATH}"
+
+# start lint ...
+make lint
