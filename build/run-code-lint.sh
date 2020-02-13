58339407c158a6355c594b3337f9245e5b0f8e70
liuwei <liuweixa@cn.ibm.com>
2020-02-13 16:51:49 +0800

    enable lint

--- build/run-code-lint.sh
+++ build/run-code-lint.sh
@@ -22,7 +22,7 @@ gem install mdl
 gem install awesome_bot
 
 # Install golangci-lint
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.23.6
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b "$(go env GOPATH)"/bin v1.23.6
 
 # Start lint task
 make lint
