cdefeeefed23fd44fa124794f4f6790f3dde5ec3
liuwei <liuweixa@cn.ibm.com>
2020-02-13 15:12:39 +0800

    enable lint

--- build/run-code-lint.sh
+++ build/run-code-lint.sh
@@ -10,11 +10,16 @@ LINT_TOOLS_PATH="${HOME}"/lint-tools
 
 mkdir -p "${LINT_TOOLS_PATH}"
 
+# install hadolint
 wget -P "${LINT_TOOLS_PATH}" https://github.com/hadolint/hadolint/releases/download/v1.17.5/hadolint-Linux-x86_64
 mv "${LINT_TOOLS_PATH}"/hadolint-Linux-x86_64 "${LINT_TOOLS_PATH}"/hadolint
 chmod +x "${LINT_TOOLS_PATH}"/hadolint
 
+# install yamllint
 pip install --user yamllint
+
+# install mdl
+gem install mdl
 
 export PATH="${LINT_TOOLS_PATH}":"${PATH}"
 
