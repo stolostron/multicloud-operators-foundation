0b4ca557a044f53cabd544be07034173bc831c05
liuwei <liuweixa@cn.ibm.com>
2020-02-13 13:19:18 +0800

    enable lint

--- build/install-dependencies.sh
+++ build/install-dependencies.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+# Install lint tools
+wget -P /usr/local/bin/hadolint https://github.com/hadolint/hadolint/releases/download/v1.17.5/hadolint-Linux-x86_64
+chmod +x /usr/local/bin/hadolint
+
 exit 0;
--- build/run-lint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Run lint
-make lint
