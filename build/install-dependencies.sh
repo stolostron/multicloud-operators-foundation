2276115767ee3492e403c3a21c186638b36718cc
liuwei <liuweixa@cn.ibm.com>
2020-02-13 14:07:15 +0800

    enable lint

--- build/install-dependencies.sh
+++ build/install-dependencies.sh
@@ -9,8 +9,6 @@ set -o xtrace
 LINT_TOOLS_PATH="${HOME}"/lint-tools
 mkdir -p "${LINT_TOOLS_PATH}"
 
-export PATH=$LINT_TOOLS_PATH:$PATH
-
 wget -P "${LINT_TOOLS_PATH}" https://github.com/hadolint/hadolint/releases/download/v1.17.5/hadolint-Linux-x86_64
 mv "${LINT_TOOLS_PATH}"/hadolint-Linux-x86_64 "${LINT_TOOLS_PATH}"/hadolint
 chmod +x "${LINT_TOOLS_PATH}"/hadolint
