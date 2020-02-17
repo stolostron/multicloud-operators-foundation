09ac336ee6b8e2e8414037ce6f0bea21e3895828
Jian Qiu <qiujian@cn.ibm.com>
2020-02-17 15:08:11 +0800

    Fix conversion bug

--- pkg/apis/mcm/v1beta1/conversion.go
+++ pkg/apis/mcm/v1beta1/conversion.go
@@ -7,5 +7,5 @@ import (
 
 // Convert_mcm_WorkSpec_To_v1beta1_WorkSpec convert v1beta1 workspec to internal version
 func Convert_mcm_WorkSpec_To_v1beta1_WorkSpec(out *mcm.WorkSpec, in *WorkSpec, s conversion.Scope) error {
-	return autoConvert_v1beta1_WorkSpec_To_mcm_WorkSpec(in, out, s)
+	return autoConvert_mcm_WorkSpec_To_v1beta1_WorkSpec(out, in, s)
 }
