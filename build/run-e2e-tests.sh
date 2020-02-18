b3fd27eb01433bccc92c553c15ad2fb6b7433f56
liuwei <liuweixa@cn.ibm.com>
2020-02-18 11:05:04 +0800

    enable e2e test

--- build/run-e2e-tests.sh
+++ build/run-e2e-tests.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
+set -o errexi
+set -o nounset
+set -o pipefail
+set -o xtrace
+
 # Run e2e test
 export IMAGE_NAME_AND_VERSION=${1}
+
+
+GO111MODULE="on" go get sigs.k8s.io/kind@v0.7.0
+
+kind create cluster
