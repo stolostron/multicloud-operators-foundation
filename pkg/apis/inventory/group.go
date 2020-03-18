b8288fde9a5dbe5185c7354f4e11af42a7c866f5
Sudarshan Acharya <sudarshan.acharya@redhat.com>
2020-03-18 09:54:12 -0500

    Renaming the inventory package name

--- pkg/apis/inventory/group.go
+++ pkg/apis/inventory/group.go
@@ -1,6 +1,6 @@
-// Package midas contains midas API versions.
+// Package inventory contains inventory API versions.
 //
-// This file ensures Go source parsers acknowledge the midas package
+// This file ensures Go source parsers acknowledge the inventory package
 // and any child packages. It can be removed if any other Go source files are
 // added to this package.
-package midas
+package inventory
