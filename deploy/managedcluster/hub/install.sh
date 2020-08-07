4ac8ddf3fcb5aaeacf4cf80a851a0f63daf7ce9f
liuwei <liuweixa@redhat.com>
2020-08-07 15:51:52 +0800

    fix managedcluster deploy

--- deploy/managedcluster/hub/install.sh
+++ deploy/managedcluster/hub/install.sh
@@ -1,31 +1,55 @@
 #!/bin/bash
 
-set -o errexi
 set -o nounset
 set -o pipefail
+
+KUBECTL=${KUBECTL:-kubectl}
+
+# On openshift, OLM is installed into openshift-operator-lifecycle-manager
+$KUBECTL get namespace openshift-operator-lifecycle-manager 1>/dev/null 2>&1
+if [ $? -eq 0 ]; then
+  export OLM_NAMESPACE=openshift-operator-lifecycle-manager
+fi
 
 rm -rf registration-operator
 
 echo "############  Cloning registration-operator"
 git clone https://github.com/open-cluster-management/registration-operator.git
+
 cd registration-operator || {
   printf "cd failed, registration-operator does not exist"
   return 1
 }
 
+# TODO: Delete this
+if [ -n "$IMAGE_NAME" ]; then
+  IMAGE_NAME_BAK="$IMAGE_NAME"
+  unset IMAGE_NAME
+fi
+
 echo "############  Deploying hub"
 make deploy-hub
+if [ $? -ne 0 ]; then
+  echo "############  Failed to deploy hub"
+  exit 1
+fi
+
+# TODO: Delete this
+if [ -n "$IMAGE_NAME_BAK" ]; then
+  export FOUNDATION_IMAGE_NAME="$IMAGE_NAME_BAK"
+  unset IMAGE_NAME_BAK
+fi
 
 for i in {1..7}; do
   echo "############$i  Checking cluster-manager-registration-controller"
-  RUNNING_POD=$(kubectl -n open-cluster-management-hub get pods | grep cluster-manager-registration-controller | grep -c "Running")
+  RUNNING_POD=$($KUBECTL -n open-cluster-management-hub get pods | grep cluster-manager-registration-controller | grep -c "Running")
   if [ "${RUNNING_POD}" -eq 3 ]; then
     break
   fi
 
   if [ $i -eq 7 ]; then
     echo "!!!!!!!!!!  the cluster-manager-registration-controller is not ready within 3 minutes"
-    kubectl -n open-cluster-management-hub get pods
+    $KUBECTL -n open-cluster-management-hub get pods
 
     exit 1
   fi
@@ -34,14 +58,14 @@ done
 
 for i in {1..7}; do
   echo "############$i  Checking cluster-manager-registration-webhook"
-  RUNNING_POD=$(kubectl -n open-cluster-management-hub get pods | grep cluster-manager-registration-webhook | grep -c "Running")
+  RUNNING_POD=$($KUBECTL -n open-cluster-management-hub get pods | grep cluster-manager-registration-webhook | grep -c "Running")
   if [ "${RUNNING_POD}" -eq 3 ]; then
     break
   fi
 
   if [ $i -eq 7 ]; then
     echo "!!!!!!!!!!  the cluster-manager-registration-webhook is not ready within 3 minutes"
-    kubectl -n open-cluster-management-hub get pods
+    $KUBECTL -n open-cluster-management-hub get pods
     exit 1
   fi
   sleep 30
--- deploy/managedcluster/klusterlet/install.sh
+++ deploy/managedcluster/klusterlet/install.sh
@@ -1,31 +1,55 @@
 #!/bin/bash
 
-set -o errexi
 set -o nounset
 set -o pipefail
+
+KUBECTL=${KUBECTL:-kubectl}
+
+# On openshift, OLM is installed into openshift-operator-lifecycle-manager
+$KUBECTL get namespace openshift-operator-lifecycle-manager 1>/dev/null 2>&1
+if [ $? -eq 0 ]; then
+  export OLM_NAMESPACE=openshift-operator-lifecycle-manager
+fi
 
 rm -rf registration-operator
 
 echo "############  Cloning registration-operator"
 git clone https://github.com/open-cluster-management/registration-operator.git
+
 cd registration-operator || {
   printf "cd failed, registration-operator does not exist"
   return 1
 }
 
+# TODO: Delete this
+if [ -n "$IMAGE_NAME" ]; then
+  IMAGE_NAME_BAK="$IMAGE_NAME"
+  unset IMAGE_NAME
+fi
+
 echo "############  Deploying klusterlet"
 make deploy-spoke
+if [ $? -ne 0 ]; then
+  echo "############  Failed to deploy klusterlet"
+  exit 1
+fi
+
+# TODO: Delete this
+if [ -n "$IMAGE_NAME_BAK" ]; then
+  export FOUNDATION_IMAGE_NAME="$IMAGE_NAME_BAK"
+  unset IMAGE_NAME_BAK
+fi
 
 for i in {1..7}; do
   echo "############$i  Checking klusterlet-registration-agent"
-  RUNNING_POD=$(kubectl -n open-cluster-management-agent get pods | grep klusterlet-registration-agent | grep -c "Running")
+  RUNNING_POD=$($KUBECTL -n open-cluster-management-agent get pods | grep klusterlet-registration-agent | grep -c "Running")
   if [ "${RUNNING_POD}" -eq 3 ]; then
     break
   fi
 
   if [ $i -eq 7 ]; then
     echo "!!!!!!!!!!  the klusterlet-registration-agent is not ready within 3 minutes"
-    kubectl -n open-cluster-management-agent get pods
+    $KUBECTL -n open-cluster-management-agent get pods
     exit 1
   fi
   sleep 30
