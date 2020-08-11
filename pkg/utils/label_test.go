dd336d54ffd2e3513239f99c78ebc2ee19af873b
daliu <daliu@redhat.com>
2020-08-11 15:37:13 +0800

    fix label test

--- pkg/utils/label_test.go
+++ pkg/utils/label_test.go
@@ -78,6 +78,34 @@ func TestMergeMap(t *testing.T) {
 		})
 	}
 }
+func TestStringToMap(t *testing.T) {
+	testCases := []struct {
+		name string
+		str  string
+		rst  map[string]string
+	}{
+		{
+			name: "case1",
+			str:  "",
+			rst:  nil,
+		},
+		{
+			name: "case2",
+			str:  "app=opt,zone=east-1",
+			rst: map[string]string{
+				"app":  "opt",
+				"zone": "east-1",
+			},
+		},
+	}
+
+	for _, testCase := range testCases {
+		rst := StringToMap(testCase.str)
+		if len(rst) != len(testCase.rst) {
+			t.Errorf("test case %s fail", testCase.name)
+		}
+	}
+}
 
 func TestStrings(t *testing.T) {
 	str := []string{"label1", "label2", "label3"}
