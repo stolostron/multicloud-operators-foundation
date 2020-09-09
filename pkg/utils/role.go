441fdedbcb8226a1c06d143143879d922d667834
daliu <daliu@redhat.com>
2020-09-09 14:31:38 +0800

    change follow review

--- pkg/utils/role.go
+++ pkg/utils/role.go
@@ -1,8 +1,11 @@
 package utils
 
 import (
+	"reflect"
+
 	clusterv1alpha1 "github.com/open-cluster-management/api/cluster/v1alpha1"
 	rbacv1 "k8s.io/api/rbac/v1"
+	"k8s.io/apimachinery/pkg/util/sets"
 )
 
 func Mergesubjects(subjects []rbacv1.Subject, cursubjects []rbacv1.Subject) []rbacv1.Subject {
@@ -23,21 +26,44 @@ func generateMapKey(subject rbacv1.Subject) string {
 	return subject.APIGroup + subject.Kind + subject.Name
 }
 
-func GetClustersetInRules(rules []rbacv1.PolicyRule) []string {
-	var clustersetNames []string
+func GetClustersetInRules(rules []rbacv1.PolicyRule) sets.String {
+	clustersetNames := sets.NewString()
 	for _, rule := range rules {
 		if ContainsString(rule.APIGroups, "*") && ContainsString(rule.Resources, "*") && ContainsString(rule.Verbs, "*") {
-			return []string{"*"}
+			clustersetNames.Insert("*")
 		}
-		if ContainsString(rule.APIGroups, clusterv1alpha1.GroupName) {
-			if ContainsString(rule.Resources, "managedclustersets/bind") {
-				if ContainsString(rule.Verbs, "create") {
-					for _, resourcename := range rule.ResourceNames {
-						clustersetNames = append(clustersetNames, resourcename)
-					}
-				}
+		if !ContainsString(rule.APIGroups, clusterv1alpha1.GroupName) {
+			continue
+		}
+		if !ContainsString(rule.Resources, "managedclustersets/bind") && !ContainsString(rule.Resources, "*") {
+			continue
+		}
+
+		if !ContainsString(rule.Verbs, "create") && !ContainsString(rule.Verbs, "*") {
+			continue
+		}
+		for _, resourcename := range rule.ResourceNames {
+			if resourcename == "*" {
+				return sets.NewString("*")
 			}
+			clustersetNames.Insert(resourcename)
 		}
 	}
 	return clustersetNames
+}
+
+func EqualSubjects(subjects1, subjects2 []rbacv1.Subject) bool {
+	if len(subjects1) != len(subjects2) {
+		return false
+	}
+	var subjectMap1 = make(map[rbacv1.Subject]bool)
+	for _, curSubject := range subjects1 {
+		subjectMap1[curSubject] = true
+	}
+
+	var subjectMap2 = make(map[rbacv1.Subject]bool)
+	for _, curSubject := range subjects2 {
+		subjectMap2[curSubject] = true
+	}
+	return reflect.DeepEqual(subjectMap1, subjectMap2)
 }
