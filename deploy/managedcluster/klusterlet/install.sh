66246e3d57c3cfc52440466ec22652172afcea1b
Zhiwei Yin <zyin@redhat.com>
2020-10-20 14:21:46 +0800

    use olm to deploy registration-operator

--- deploy/managedcluster/klusterlet/install.sh
+++ deploy/managedcluster/klusterlet/install.sh
@@ -25,32 +25,11 @@ cd registration-operator || {
 }
 
 echo "############  Deploying klusterlet"
-#make deploy-spoke
-#if [ $? -ne 0 ]; then
-#  echo "############  Failed to deploy klusterlet"
-#  exit 1
-#fi
-
-
-CLUSTER_IP=$($KUBECTL get svc kubernetes -n default -o jsonpath="{.spec.clusterIP}")
-cp ${KUBECONFIG} dev-kubeconfig
-$KUBECTL config use-context ${KLUSTERLET_KUBECONFIG_CONTEXT}
-
-ns=$($KUBECTL get ns open-cluster-management-agent | grep -c "open-cluster-management-agent")
-if [ "${ns}" -eq 0 ]; then
-  $KUBECTL create ns open-cluster-management-agent
+make deploy-spoke
+if [ $? -ne 0 ]; then
+ echo "############  Failed to deploy klusterlet"
+ exit 1
 fi
-
-$KUBECTL config set clusters.kind-${KIND_CLUSTER}.server https://${CLUSTER_IP} --kubeconfig dev-kubeconfig
-$KUBECTL delete secret bootstrap-hub-kubeconfig -n open-cluster-management-agent --ignore-not-found
-$KUBECTL create secret generic bootstrap-hub-kubeconfig --from-file=kubeconfig=dev-kubeconfig -n open-cluster-management-agent
-
-$KUBECTL apply -f deploy/klusterlet/crds/0000_00_operator.open-cluster-management.io_klusterlets.crd.yaml
-$KUBECTL apply -f deploy/klusterlet/cluster_role.yaml
-$KUBECTL apply -f deploy/klusterlet/cluster_role_binding.yaml
-$KUBECTL apply -f deploy/klusterlet/service_account.yaml
-$KUBECTL apply -f deploy/klusterlet/operator.yaml
-$KUBECTL apply -f deploy/klusterlet/crds/operator_open-cluster-management_klusterlets.cr.yaml
 
 for i in {1..7}; do
   echo "############$i  Checking klusterlet-registration-agent"
