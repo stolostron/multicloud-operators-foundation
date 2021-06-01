63c26eb7653908710893619b6793db443a47af62
ldpliu <liudangpeng@126.com>
2021-06-01 11:29:19 +0800

    add more case for clusterset rbac e2e

--- test/e2e/util/template.go
+++ test/e2e/util/template.go
@@ -1,5 +1,135 @@
 package util
 
+import (
+	clusterv1alpha1 "github.com/open-cluster-management/api/cluster/v1alpha1"
+	hivev1 "github.com/openshift/hive/apis/hive/v1"
+	"github.com/openshift/hive/apis/hive/v1/aws"
+	v1 "k8s.io/api/core/v1"
+	rbacv1 "k8s.io/api/rbac/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+var NamespaceRandomTemplate = &v1.Namespace{
+	ObjectMeta: metav1.ObjectMeta{
+		GenerateName: "test-automation-",
+		Labels: map[string]string{
+			"test-automation": "true",
+		},
+	},
+}
+
+var ManagedClusterSetRandomTemplate = &clusterv1alpha1.ManagedClusterSet{
+	ObjectMeta: metav1.ObjectMeta{
+		GenerateName: "test-automation-clusterset-",
+	},
+	Spec: clusterv1alpha1.ManagedClusterSetSpec{},
+}
+var ManagedClusterSetTemplate = &clusterv1alpha1.ManagedClusterSet{
+	ObjectMeta: metav1.ObjectMeta{
+		Name: "clusterset1",
+	},
+	Spec: clusterv1alpha1.ManagedClusterSetSpec{},
+}
+
+var ClusterRoleBindingAdminTemplate = &rbacv1.ClusterRoleBinding{
+	ObjectMeta: metav1.ObjectMeta{
+		Name: "clustersetrolebindingAdmin",
+	},
+	RoleRef: rbacv1.RoleRef{
+		APIGroup: "rbac.authorization.k8s.io",
+		Kind:     "ClusterRole",
+		Name:     "open-cluster-management:managedclusterset:admin:clusterset1",
+	},
+	Subjects: []rbacv1.Subject{
+		{
+			Kind:     "User",
+			APIGroup: "rbac.authorization.k8s.io",
+			Name:     "admin1",
+		},
+	},
+}
+
+var ClusterRoleBindingViewTemplate = &rbacv1.ClusterRoleBinding{
+	ObjectMeta: metav1.ObjectMeta{
+		Name: "clustersetrolebindingView",
+	},
+	RoleRef: rbacv1.RoleRef{
+		APIGroup: "rbac.authorization.k8s.io",
+		Kind:     "ClusterRole",
+		Name:     "open-cluster-management:managedclusterset:view:clusterset1",
+	},
+	Subjects: []rbacv1.Subject{
+		{
+			Kind:     "User",
+			APIGroup: "rbac.authorization.k8s.io",
+			Name:     "view1",
+		},
+	},
+}
+
+var ClusterpoolTemplate = &hivev1.ClusterPool{
+	ObjectMeta: metav1.ObjectMeta{
+		Name:      "clusterpool1",
+		Namespace: "default",
+		Labels: map[string]string{
+			"cluster.open-cluster-management.io/clusterset": "clusterset1",
+		},
+	},
+	Spec: hivev1.ClusterPoolSpec{
+		BaseDomain: "dev04.red-chesterfield.com",
+		ImageSetRef: hivev1.ClusterImageSetReference{
+			Name: "img4.6.29-x86-64-appsub",
+		},
+		Platform: hivev1.Platform{
+			AWS: &aws.Platform{
+				CredentialsSecretRef: v1.LocalObjectReference{
+					Name: "aws-clusterpool-aws-creds",
+				},
+				Region: "us-east",
+			},
+		},
+		Size: 2,
+	},
+}
+
+var ClusterclaimTemplate = &hivev1.ClusterClaim{
+	ObjectMeta: metav1.ObjectMeta{
+		Name:      "clusterclaim1",
+		Namespace: "default",
+	},
+	Spec: hivev1.ClusterClaimSpec{
+		ClusterPoolName: "clusterpool1",
+	},
+}
+var ClusterdeploymentTemplate = &hivev1.ClusterDeployment{
+	ObjectMeta: metav1.ObjectMeta{
+		Name: "clusterdeployment1",
+	},
+	Spec: hivev1.ClusterDeploymentSpec{
+		BaseDomain: "dev04.red-chesterfield.com",
+		ClusterPoolRef: &hivev1.ClusterPoolReference{
+			Namespace: "default",
+			PoolName:  "clusterpool1",
+		},
+		Platform: hivev1.Platform{
+			AWS: &aws.Platform{
+				CredentialsSecretRef: v1.LocalObjectReference{
+					Name: "aws-clusterpool-aws-creds",
+				},
+				Region: "us-east",
+			},
+		},
+	},
+}
+
+const ManagedClusterSetRandomTemplateJson = `{
+  "apiVersion": "cluster.open-cluster-management.io/v1alpha1",
+  "kind": "ManagedClusterSet",
+  "metadata": {
+    "generateName": "test-automation-clusterset-"
+  }
+}`
+
 // ManagedClusterActionCreateTemplate is json template for create action
 const ManagedClusterActionCreateTemplate = `{
   "apiVersion": "action.open-cluster-management.io/v1beta1",
@@ -179,42 +309,6 @@ const ManagedClusterViewTemplate = `{
       "namespace": "open-cluster-management-agent"
     }
   }
-}`
-
-const ManagedClusterSetTemplate = `{
-  "apiVersion": "cluster.open-cluster-management.io/v1alpha1",
-  "kind": "ManagedClusterSet",
-  "metadata": {
-    "name": "clusterset1"
-  }
-}`
-
-const ManagedClusterSetRandomTemplate = `{
-  "apiVersion": "cluster.open-cluster-management.io/v1alpha1",
-  "kind": "ManagedClusterSet",
-  "metadata": {
-    "generateName": "test-automation-clusterset-"
-  }
-}`
-
-const ClusterRoleBindingTemplate = `{
-  "apiVersion": "rbac.authorization.k8s.io/v1",
-    "kind": "ClusterRoleBinding",
-    "metadata": {
-        "name": "clustersetrolebinding1"
-    },
-    "roleRef": {
-        "apiGroup": "rbac.authorization.k8s.io",
-        "kind": "ClusterRole",
-        "name": "open-cluster-management:managedclusterset:admin:clusterset1"
-    },
-    "subjects": [
-        {
-            "apiGroup": "rbac.authorization.k8s.io",
-            "kind": "User",
-            "name": "user1"
-        }
-    ]
 }`
 
 const BMATemplate = `
