3b49233a5d4943523acf5488a5e1345df6d70913
haoqing0110 <qhao@redhat.com>
2022-07-20 06:55:44 +0000

    remove unhelpful testing code
    Signed-off-by: haoqing0110 <qhao@redhat.com>

--- pkg/helpers/rbac_helpers_test.go
+++ pkg/helpers/rbac_helpers_test.go
@@ -8,9 +8,6 @@ import (
 
 func TestRule(t *testing.T) {
 	NewRule("get", "list", "watch").Names("test").Groups(clusterv1beta1.GroupName).Resources("managedclusteractions").RuleOrDie()
-	NewRule("get", "list", "watch").Names("test").Groups(clusterv1beta1.GroupName).Resources("managedclusteractions").URLs("xx").Rule()
 	NewClusterBinding("test").Groups(clusterv1beta1.GroupName).Users("admin").SAs("default", "admin_sa").BindingOrDie()
-	NewClusterBinding("test").Groups(clusterv1beta1.GroupName).Users("admin").SAs("default", "admin_sa").Binding()
 	NewRoleBinding("clusterName", "clusterNamespace").Users("hcm:clusters:" + "clusterNamespace" + ":" + "clusterName").BindingOrDie()
-	NewRoleBinding("clusterName", "clusterNamespace").Users("hcm:clusters:" + "clusterNamespace" + ":" + "clusterName").Binding()
 }
