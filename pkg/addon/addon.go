336896a6b962e9ce6abe4eeeb413f7e7f2586561
Wei Liu <liuweixa@redhat.com>
2022-07-27 14:33:43 +0800

    urollback nodeselector and toleratiions (#518)
    Signed-off-by: Wei Liu <liuweixa@redhat.com>

--- pkg/addon/addon.go
+++ pkg/addon/addon.go
@@ -4,17 +4,13 @@ import (
 	"context"
 	"embed"
 	"encoding/json"
-	"fmt"
 	"reflect"
-	"strings"
 
 	"github.com/stolostron/cluster-lifecycle-api/helpers/imageregistry"
 	"github.com/stolostron/multicloud-operators-foundation/pkg/helpers"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	utilerrors "k8s.io/apimachinery/pkg/util/errors"
-	"k8s.io/apimachinery/pkg/util/validation"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/klog"
 	"open-cluster-management.io/addon-framework/pkg/addonfactory"
@@ -31,11 +27,8 @@ const (
 	clusterRoleName = "managed-cluster-workmgr"
 	roleBindingName = "managed-cluster-workmgr"
 
-	// annotationNodeSelector is key name of nodeSelector annotation on ManagedCluster
+	// annotationNodeSelector is key name of nodeSelector annotation synced from mch
 	annotationNodeSelector = "open-cluster-management/nodeSelector"
-
-	// annotationValues is key name of tolerations annotation on ManagedCluster
-	annotationTolerations = "open-cluster-management/tolerations"
 )
 
 //go:embed manifests
@@ -53,11 +46,10 @@ type GlobalValues struct {
 }
 
 type Values struct {
-	Product                         string              `json:"product,omitempty"`
-	Tolerations                     []corev1.Toleration `json:"tolerations,omitempty"`
-	GlobalValues                    GlobalValues        `json:"global,omitempty,omitempty"`
-	EnableSyncLabelsToClusterClaims bool                `json:"enableSyncLabelsToClusterClaims"`
-	EnableNodeCollector             bool                `json:"enableNodeCollector"`
+	Product                         string       `json:"product,omitempty"`
+	GlobalValues                    GlobalValues `json:"global,omitempty,omitempty"`
+	EnableSyncLabelsToClusterClaims bool         `json:"enableSyncLabelsToClusterClaims"`
+	EnableNodeCollector             bool         `json:"enableNodeCollector"`
 }
 
 func NewGetValuesFunc(imageName string) addonfactory.GetValuesFunc {
@@ -103,14 +95,6 @@ func NewGetValuesFunc(imageName string) addonfactory.GetValuesFunc {
 		}
 		if len(nodeSelector) != 0 {
 			addonValues.GlobalValues.NodeSelector = nodeSelector
-		}
-
-		tolerations, err := getTolerations(cluster)
-		if err != nil {
-			return nil, err
-		}
-		if len(tolerations) != 0 {
-			addonValues.Tolerations = tolerations
 		}
 
 		values, err := addonfactory.JsonStructToValues(addonValues)
@@ -165,103 +149,15 @@ func createOrUpdateRoleBinding(kubeClient kubernetes.Interface, addonName, clust
 func getNodeSelector(managedCluster *clusterv1.ManagedCluster) (map[string]string, error) {
 	nodeSelector := map[string]string{}
 
-	nodeSelectorString, ok := managedCluster.Annotations[annotationNodeSelector]
-	if !ok {
-		return nodeSelector, nil
-	}
-
-	if err := json.Unmarshal([]byte(nodeSelectorString), &nodeSelector); err != nil {
-		return nil, fmt.Errorf("invalid nodeSelector annotation of cluster %s, %v", managedCluster.Name, err)
-	}
-
-	if err := validateNodeSelector(nodeSelector); err != nil {
-		return nil, fmt.Errorf("invalid nodeSelector annotation of cluster %s, %v", managedCluster.Name, err)
+	if managedCluster.GetName() == "local-cluster" {
+		annotations := managedCluster.GetAnnotations()
+		if nodeSelectorString, ok := annotations[annotationNodeSelector]; ok {
+			if err := json.Unmarshal([]byte(nodeSelectorString), &nodeSelector); err != nil {
+				klog.Error(err, "failed to unmarshal nodeSelector annotation of cluster %v", managedCluster.GetName())
+				return nodeSelector, err
+			}
+		}
 	}
 
 	return nodeSelector, nil
-}
-
-// refer to https://github.com/kubernetes/kubernetes/blob/master/pkg/apis/core/validation/validation.go#L3498
-func validateNodeSelector(nodeSelector map[string]string) error {
-	errs := []error{}
-	for key, val := range nodeSelector {
-		if errMsgs := validation.IsQualifiedName(key); len(errMsgs) != 0 {
-			errs = append(errs, fmt.Errorf(strings.Join(errMsgs, ";")))
-		}
-		if errMsgs := validation.IsValidLabelValue(val); len(errMsgs) != 0 {
-			errs = append(errs, fmt.Errorf(strings.Join(errMsgs, ";")))
-		}
-	}
-	return utilerrors.NewAggregate(errs)
-}
-
-func getTolerations(cluster *clusterv1.ManagedCluster) ([]corev1.Toleration, error) {
-	tolerations := []corev1.Toleration{}
-
-	tolerationsString, ok := cluster.Annotations[annotationTolerations]
-	if !ok {
-		return tolerations, nil
-	}
-
-	if err := json.Unmarshal([]byte(tolerationsString), &tolerations); err != nil {
-		return nil, fmt.Errorf("invalid tolerations annotation of cluster %s, %v", cluster.Name, err)
-	}
-
-	if err := validateTolerations(tolerations); err != nil {
-		return nil, fmt.Errorf("invalid tolerations annotation of cluster %s, %v", cluster.Name, err)
-	}
-
-	return tolerations, nil
-}
-
-// refer to https://github.com/kubernetes/kubernetes/blob/master/pkg/apis/core/validation/validation.go#L3330
-func validateTolerations(tolerations []corev1.Toleration) error {
-	errs := []error{}
-	for _, toleration := range tolerations {
-		// validate the toleration key
-		if len(toleration.Key) > 0 {
-			if errMsgs := validation.IsQualifiedName(toleration.Key); len(errMsgs) != 0 {
-				errs = append(errs, fmt.Errorf(strings.Join(errMsgs, ";")))
-			}
-		}
-
-		// empty toleration key with Exists operator and empty value means match all taints
-		if len(toleration.Key) == 0 && toleration.Operator != corev1.TolerationOpExists {
-			if len(toleration.Operator) == 0 {
-				errs = append(errs, fmt.Errorf(
-					"operator must be Exists when `key` is empty, which means \"match all values and all keys\""))
-			}
-		}
-
-		if toleration.TolerationSeconds != nil && toleration.Effect != corev1.TaintEffectNoExecute {
-			errs = append(errs, fmt.Errorf("effect must be 'NoExecute' when `tolerationSeconds` is set"))
-		}
-
-		// validate toleration operator and value
-		switch toleration.Operator {
-		// empty operator means Equal
-		case corev1.TolerationOpEqual, "":
-			if errMsgs := validation.IsValidLabelValue(toleration.Value); len(errMsgs) != 0 {
-				errs = append(errs, fmt.Errorf(strings.Join(errMsgs, ";")))
-			}
-		case corev1.TolerationOpExists:
-			if len(toleration.Value) > 0 {
-				errs = append(errs, fmt.Errorf("value must be empty when `operator` is 'Exists'"))
-			}
-		default:
-			errs = append(errs, fmt.Errorf("the operator %q is not supported", toleration.Operator))
-		}
-
-		// validate toleration effect, empty toleration effect means match all taint effects
-		if len(toleration.Effect) > 0 {
-			switch toleration.Effect {
-			case corev1.TaintEffectNoSchedule, corev1.TaintEffectPreferNoSchedule, corev1.TaintEffectNoExecute:
-				// allowed values are NoSchedule, PreferNoSchedule and NoExecute
-			default:
-				errs = append(errs, fmt.Errorf("the effect %q is not supported", toleration.Effect))
-			}
-		}
-	}
-
-	return utilerrors.NewAggregate(errs)
 }
