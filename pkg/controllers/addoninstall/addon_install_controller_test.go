665b3b7d0fa61f5c8d505fc722030e271a06fa14
Yang Le <yangle@redhat.com>
2022-11-22 18:34:55 +0800

    install work-manager outside of addon manager
    Signed-off-by: Yang Le <yangle@redhat.com>

---
+++ pkg/controllers/addoninstall/addon_install_controller_test.go
@@ -0,0 +1,166 @@
+package addoninstall
+
+import (
+	"context"
+	"fmt"
+	"reflect"
+	"testing"
+
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/sets"
+	kubescheme "k8s.io/client-go/kubernetes/scheme"
+	"open-cluster-management.io/addon-framework/pkg/addonmanager/constants"
+	addonapiv1alpha1 "open-cluster-management.io/api/addon/v1alpha1"
+	clusterv1 "open-cluster-management.io/api/cluster/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+)
+
+var (
+	scheme                 = runtime.NewScheme()
+	logCertSecretNamespace = "open-cluster-management"
+	logCertSecretName      = "ocm-klusterlet-self-signed-secrets"
+)
+
+func newTestReconciler(addOnName string, existingObjs ...runtime.Object) *Reconciler {
+	s := kubescheme.Scheme
+	clusterv1.Install(s)
+	addonapiv1alpha1.Install(s)
+
+	client := fake.NewClientBuilder().WithScheme(s).WithRuntimeObjects(existingObjs...).Build()
+	return &Reconciler{
+		client:     client,
+		scheme:     scheme,
+		addOnNames: sets.NewString(addOnName),
+	}
+}
+
+func newManagedCluster(name string, annotations map[string]string) *clusterv1.ManagedCluster {
+	return &clusterv1.ManagedCluster{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:        name,
+			Annotations: annotations,
+		},
+	}
+}
+
+func newManagedClusterAddOn(addOnName, clusterName string, annotations map[string]string) *addonapiv1alpha1.ManagedClusterAddOn {
+	return &addonapiv1alpha1.ManagedClusterAddOn{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:        addOnName,
+			Namespace:   clusterName,
+			Annotations: annotations,
+		},
+	}
+}
+
+func TestReconciler(t *testing.T) {
+	addOnName := "test-addon"
+	clusterName := "cluster1"
+	ctx := context.TODO()
+
+	tests := []struct {
+		name                       string
+		existingCluster            *clusterv1.ManagedCluster
+		existingAddOn              *addonapiv1alpha1.ManagedClusterAddOn
+		enabled                    bool
+		expectedInstallNamespace   string
+		expectedHostingClusterName string
+	}{
+		{
+			name: "no cluster",
+		},
+		{
+			name: "cluster with add-on disabled",
+			existingCluster: newManagedCluster(clusterName, map[string]string{
+				constants.DisableAddonAutomaticInstallationAnnotationKey: "true",
+			}),
+		},
+		{
+			name:            "add-on exists",
+			existingCluster: newManagedCluster(clusterName, nil),
+			existingAddOn: newManagedClusterAddOn(addOnName, clusterName, map[string]string{
+				AnnotationAddOnHostingClusterName: "hosting-cluster",
+			}),
+		},
+		{
+			name:                     "cluster in default mode",
+			existingCluster:          newManagedCluster(clusterName, nil),
+			enabled:                  true,
+			expectedInstallNamespace: DefaultAddOnInstallNamespace,
+		},
+		{
+			name: "cluster in hosted mode with add-on in default mode",
+			existingCluster: newManagedCluster(clusterName, map[string]string{
+				AnnotationKlusterletDeployMode:         "Hosted",
+				AnnotationKlusterletHostingClusterName: "local-cluster",
+			}),
+			enabled:                  true,
+			expectedInstallNamespace: DefaultAddOnInstallNamespace,
+		},
+		{
+			name: "cluster in hosted mode with add-on in host mode",
+			existingCluster: newManagedCluster(clusterName, map[string]string{
+				AnnotationKlusterletDeployMode:         "Hosted",
+				AnnotationKlusterletHostingClusterName: "local-cluster",
+				AnnotationEnableHostedModeAddons:       "true",
+			}),
+			enabled:                    true,
+			expectedInstallNamespace:   fmt.Sprintf("klusterlet-%s", clusterName),
+			expectedHostingClusterName: "local-cluster",
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			existingObjs := []runtime.Object{}
+			if test.existingCluster != nil {
+				existingObjs = append(existingObjs, test.existingCluster)
+			}
+			if test.existingAddOn != nil {
+				existingObjs = append(existingObjs, test.existingAddOn)
+			}
+
+			r := newTestReconciler(addOnName, existingObjs...)
+			_, err := r.Reconcile(ctx, reconcile.Request{NamespacedName: types.NamespacedName{Name: clusterName}})
+			if err != nil {
+				t.Errorf("unexpected error :%v", err)
+			}
+
+			addOn := &addonapiv1alpha1.ManagedClusterAddOn{}
+			err = r.client.Get(ctx, types.NamespacedName{Name: addOnName, Namespace: clusterName}, addOn)
+
+			switch {
+			case test.existingAddOn != nil:
+				if err != nil {
+					t.Errorf("unexpected error :%v", err)
+				}
+
+				if !reflect.DeepEqual(addOn.ObjectMeta, test.existingAddOn.ObjectMeta) {
+					t.Errorf("expect addon cr \n%v but got: \n%v", test.existingAddOn, addOn)
+				}
+
+				if !reflect.DeepEqual(addOn.Spec, test.existingAddOn.Spec) {
+					t.Errorf("expect addon cr \n%v but got: \n%v", test.existingAddOn, addOn)
+				}
+			case test.enabled:
+				if err != nil {
+					t.Errorf("unexpected error :%v", err)
+				}
+				if addOn.Spec.InstallNamespace != test.expectedInstallNamespace {
+					t.Errorf("expect install namespace %q but got: %v", test.expectedInstallNamespace, addOn.Spec.InstallNamespace)
+				}
+
+				if value := addOn.Annotations[AnnotationAddOnHostingClusterName]; value != test.expectedHostingClusterName {
+					t.Errorf("expect hosting cluster %q but got: %v", test.expectedInstallNamespace, value)
+				}
+			case err == nil:
+				t.Errorf("should not create add-on cr")
+			case !errors.IsNotFound(err):
+				t.Errorf("unexpected error :%v", err)
+			}
+		})
+	}
+}
---
+++ test/e2e/addoninstall_test.go
@@ -0,0 +1,149 @@
+package e2e
+
+import (
+	"context"
+	"fmt"
+	"reflect"
+
+	"github.com/onsi/ginkgo/v2"
+	"github.com/onsi/gomega"
+	"github.com/stolostron/multicloud-operators-foundation/pkg/controllers/addoninstall"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/rand"
+	"open-cluster-management.io/addon-framework/pkg/addonmanager/constants"
+	addonapiv1alpha1 "open-cluster-management.io/api/addon/v1alpha1"
+	clusterv1 "open-cluster-management.io/api/cluster/v1"
+)
+
+var _ = ginkgo.Describe("Testing installation of work-manager add-on", func() {
+	var clusterName string
+	var annotations map[string]string
+	addonName := "work-manager"
+	hostingClusterName := "local-cluster"
+
+	ginkgo.JustBeforeEach(func() {
+		cluster, err := clusterClient.ClusterV1().ManagedClusters().Get(context.Background(), clusterName, metav1.GetOptions{})
+		if errors.IsNotFound(err) {
+			cluster = &clusterv1.ManagedCluster{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:        clusterName,
+					Annotations: annotations,
+				},
+				Spec: clusterv1.ManagedClusterSpec{
+					HubAcceptsClient: true,
+				},
+			}
+			_, err = clusterClient.ClusterV1().ManagedClusters().Create(context.Background(), cluster, metav1.CreateOptions{})
+			gomega.Expect(err).ToNot(gomega.HaveOccurred())
+			return
+		}
+
+		gomega.Expect(err).ToNot(gomega.HaveOccurred())
+		if reflect.DeepEqual(cluster.Annotations, annotations) {
+			return
+		}
+
+		cluster = cluster.DeepCopy()
+		cluster.Annotations = annotations
+		_, err = clusterClient.ClusterV1().ManagedClusters().Update(context.Background(), cluster, metav1.UpdateOptions{})
+		gomega.Expect(err).ToNot(gomega.HaveOccurred())
+	})
+
+	ginkgo.JustAfterEach(func() {
+		err := clusterClient.ClusterV1().ManagedClusters().Delete(context.Background(), clusterName, metav1.DeleteOptions{})
+		gomega.Expect(err).ToNot(gomega.HaveOccurred())
+	})
+
+	haveNoAddonIt := func() {
+		ginkgo.It("should have no add-on installed", ginkgo.Offset(1), func() {
+			gomega.Consistently(func() bool {
+				_, err := addonClient.AddonV1alpha1().ManagedClusterAddOns(clusterName).Get(context.Background(), addonName, metav1.GetOptions{})
+				return errors.IsNotFound(err)
+			}, 30, 2).Should(gomega.BeTrue())
+		})
+	}
+
+	haveAddonInDefaultModeIt := func() {
+		ginkgo.It("should have add-on installed in default mode", ginkgo.Offset(1), func() {
+			var addon *addonapiv1alpha1.ManagedClusterAddOn
+			var err error
+			gomega.Eventually(func() error {
+				addon, err = addonClient.AddonV1alpha1().ManagedClusterAddOns(clusterName).Get(context.Background(), addonName, metav1.GetOptions{})
+				return err
+			}, eventuallyTimeout, eventuallyInterval).ShouldNot(gomega.HaveOccurred())
+
+			gomega.Expect(addon).ShouldNot(gomega.BeNil())
+			gomega.Expect(addon.Annotations).ShouldNot(gomega.HaveKey(addoninstall.AnnotationKlusterletHostingClusterName))
+			gomega.Expect(addon.Spec.InstallNamespace).To(gomega.Equal(addoninstall.DefaultAddOnInstallNamespace))
+		})
+	}
+
+	ginkgo.Context("cluster is imported in default mode", func() {
+		ginkgo.When("add-on installation is disabled", func() {
+			ginkgo.BeforeEach(func() {
+				clusterName = fmt.Sprintf("cluster-default-none-%s", rand.String(5))
+				annotations = map[string]string{
+					constants.DisableAddonAutomaticInstallationAnnotationKey: "true",
+				}
+			})
+
+			haveNoAddonIt()
+		})
+
+		ginkgo.When("default add-on installation is enabled", func() {
+			ginkgo.BeforeEach(func() {
+				clusterName = fmt.Sprintf("cluster-default-default-%s", rand.String(5))
+				annotations = nil
+			})
+
+			haveAddonInDefaultModeIt()
+		})
+	})
+
+	ginkgo.Context("cluster is imported in hosted mode", func() {
+		ginkgo.BeforeEach(func() {
+			annotations = map[string]string{
+				addoninstall.AnnotationKlusterletDeployMode:         "Hosted",
+				addoninstall.AnnotationKlusterletHostingClusterName: hostingClusterName,
+			}
+		})
+
+		ginkgo.When("add-on installation is disabled", func() {
+			ginkgo.BeforeEach(func() {
+				clusterName = fmt.Sprintf("cluster-hosted-none-%s", rand.String(5))
+				annotations[constants.DisableAddonAutomaticInstallationAnnotationKey] = "true"
+			})
+
+			haveNoAddonIt()
+		})
+
+		ginkgo.When("default add-on installation is enabled", func() {
+			ginkgo.BeforeEach(func() {
+				clusterName = fmt.Sprintf("cluster-hosted-default-%s", rand.String(5))
+			})
+
+			haveAddonInDefaultModeIt()
+		})
+
+		ginkgo.When("hosed add-on installation is enabled", func() {
+			ginkgo.BeforeEach(func() {
+				clusterName = fmt.Sprintf("cluster-hosted-hosted-%s", rand.String(5))
+				annotations[addoninstall.AnnotationEnableHostedModeAddons] = "true"
+			})
+
+			ginkgo.It("should have add-on installed in hosted mode", func() {
+				var addon *addonapiv1alpha1.ManagedClusterAddOn
+				var err error
+				gomega.Eventually(func() error {
+					addon, err = addonClient.AddonV1alpha1().ManagedClusterAddOns(clusterName).Get(context.Background(), addonName, metav1.GetOptions{})
+					return err
+				}, eventuallyTimeout, eventuallyInterval).ShouldNot(gomega.HaveOccurred())
+
+				gomega.Expect(addon).ShouldNot(gomega.BeNil())
+				gomega.Expect(addon.Annotations).To(gomega.HaveKeyWithValue(addoninstall.AnnotationAddOnHostingClusterName, hostingClusterName))
+				gomega.Expect(addon.Spec.InstallNamespace).To(gomega.Equal(fmt.Sprintf("klusterlet-%s", clusterName)))
+			})
+		})
+	})
+})
