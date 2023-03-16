b26d0cc1610b8b55e730099002aa7fa173856944
ldpliu <daliu@redhat.com>
2023-03-16 17:06:43 +0800

    make LeaseDuration/RenewDeadline/RetryPeriod configuable
    Signed-off-by: ldpliu <daliu@redhat.com>

--- cmd/controller/app/options/options.go
+++ cmd/controller/app/options/options.go
@@ -3,6 +3,8 @@
 package options
 
 import (
+	"time"
+
 	"github.com/spf13/pflag"
 )
 
@@ -13,6 +15,9 @@ type ControllerRunOptions struct {
 	LogCertSecret         string
 	EnableInventory       bool
 	EnableLeaderElection  bool
+	LeaseDuration         time.Duration
+	RenewDeadline         time.Duration
+	RetryPeriod           time.Duration
 	EnableAddonDeploy     bool
 	AddonImage            string
 	AddonInstallNamespace string
@@ -49,6 +54,19 @@ func (o *ControllerRunOptions) AddFlags(fs *pflag.FlagSet) {
 	fs.BoolVar(&o.EnableLeaderElection, "enable-leader-election", o.EnableLeaderElection,
 		"Enable leader election for controller manager. "+
 			"Enabling this will ensure there is only one active controller manager.")
+	fs.DurationVar(&o.LeaseDuration, "leader-election-lease-duration", 15*time.Second, ""+
+		"The duration that non-leader candidates will wait after observing a leadership "+
+		"renewal until attempting to acquire leadership of a led but unrenewed leader "+
+		"slot. This is effectively the maximum duration that a leader can be stopped "+
+		"before it is replaced by another candidate. This is only applicable if leader "+
+		"election is enabled.")
+	fs.DurationVar(&o.RenewDeadline, "leader-election-renew-deadline", 10*time.Second, ""+
+		"The interval between attempts by the acting master to renew a leadership slot "+
+		"before it stops leading. This must be less than or equal to the lease duration. "+
+		"This is only applicable if leader election is enabled.")
+	fs.DurationVar(&o.RetryPeriod, "leader-election-retry-period", 2*time.Second, ""+
+		"The duration the clients should wait between attempting acquisition and renewal "+
+		"of a leadership. This is only applicable if leader election is enabled.")
 	fs.BoolVar(&o.EnableAddonDeploy, "enable-agent-deploy", o.EnableAddonDeploy,
 		"Enable deploy addon agent.")
 	fs.StringVar(&o.AddonImage, "agent-addon-image", o.AddonImage,
