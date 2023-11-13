e072d76f5e90b8c22035bb0842bf2e8e7739b7aa
Jian Zhu <jiazhu@redhat.com>
2023-11-13 19:05:41 +0800

    Comment out tests that may cause ocp api server to degrade (#688)
    Signed-off-by: zhujian <jiazhu@redhat.com>

--- test/e2e/managedcluster_test.go
+++ test/e2e/managedcluster_test.go
@@ -3,12 +3,10 @@ package e2e
 import (
 	"context"
 	"fmt"
-	"net/url"
 	"reflect"
 
 	"github.com/onsi/ginkgo/v2"
 	"github.com/onsi/gomega"
-	configv1 "github.com/openshift/api/config/v1"
 	"github.com/stolostron/multicloud-operators-foundation/pkg/utils"
 	e2eutil "github.com/stolostron/multicloud-operators-foundation/test/e2e/util"
 	"k8s.io/apimachinery/pkg/api/errors"
@@ -34,67 +32,67 @@ var _ = ginkgo.Describe("Testing ManagedCluster", func() {
 	})
 
 	ginkgo.Context("Testing Clusterca sync", func() {
-		ginkgo.It("Get CA from apiserver", func() {
-			//Only need to test this case in ocp
-			if !isOcp {
-				return
-			}
-			//Create a fake secret for apiserver
-			fakesecretName := "fake-server-secret"
-			fakeSecret, err := e2eutil.CreateFakeTlsSecret(kubeClient, fakesecretName, utils.OpenshiftConfigNamespace)
-			gomega.Expect(err).ToNot(gomega.HaveOccurred())
+		// ginkgo.It("Get CA from apiserver", func() {
+		// 	//Only need to test this case in ocp
+		// 	if !isOcp {
+		// 		return
+		// 	}
+		// 	//Create a fake secret for apiserver
+		// 	fakesecretName := "fake-server-secret"
+		// 	fakeSecret, err := e2eutil.CreateFakeTlsSecret(kubeClient, fakesecretName, utils.OpenshiftConfigNamespace)
+		// 	gomega.Expect(err).ToNot(gomega.HaveOccurred())
 
-			//get apiserveraddress
-			apiserverAddress, err := utils.GetKubeAPIServerAddress(context.TODO(), ocpClient)
-			gomega.Expect(err).ToNot(gomega.HaveOccurred())
+		// 	//get apiserveraddress
+		// 	apiserverAddress, err := utils.GetKubeAPIServerAddress(context.TODO(), ocpClient)
+		// 	gomega.Expect(err).ToNot(gomega.HaveOccurred())
 
-			//add serving secret in apiserver
-			url, err := url.Parse(apiserverAddress)
-			gomega.Expect(err).ToNot(gomega.HaveOccurred())
+		// 	//add serving secret in apiserver
+		// 	url, err := url.Parse(apiserverAddress)
+		// 	gomega.Expect(err).ToNot(gomega.HaveOccurred())
 
-			apiserver, err := ocpClient.ConfigV1().APIServers().Get(context.TODO(), utils.ApiserverConfigName, metav1.GetOptions{})
-			gomega.Expect(err).ToNot(gomega.HaveOccurred())
+		// 	apiserver, err := ocpClient.ConfigV1().APIServers().Get(context.TODO(), utils.ApiserverConfigName, metav1.GetOptions{})
+		// 	gomega.Expect(err).ToNot(gomega.HaveOccurred())
 
-			newApiserver := apiserver.DeepCopy()
-			newApiserver.Spec.ServingCerts.NamedCertificates = []configv1.APIServerNamedServingCert{
-				{
-					Names: []string{
-						url.Hostname(),
-					},
-					ServingCertificate: configv1.SecretNameReference{
-						Name: fakesecretName,
-					},
-				},
-			}
+		// 	newApiserver := apiserver.DeepCopy()
+		// 	newApiserver.Spec.ServingCerts.NamedCertificates = []configv1.APIServerNamedServingCert{
+		// 		{
+		// 			Names: []string{
+		// 				url.Hostname(),
+		// 			},
+		// 			ServingCertificate: configv1.SecretNameReference{
+		// 				Name: fakesecretName,
+		// 			},
+		// 		},
+		// 	}
 
-			newApiserver, err = ocpClient.ConfigV1().APIServers().Update(context.TODO(), newApiserver, metav1.UpdateOptions{})
-			gomega.Expect(err).ToNot(gomega.HaveOccurred())
+		// 	newApiserver, err = ocpClient.ConfigV1().APIServers().Update(context.TODO(), newApiserver, metav1.UpdateOptions{})
+		// 	gomega.Expect(err).ToNot(gomega.HaveOccurred())
 
-			gomega.Eventually(func() bool {
-				cluster, err := clusterClient.ClusterV1().ManagedClusters().Get(context.Background(), defaultManagedCluster, metav1.GetOptions{})
-				gomega.Expect(err).ToNot(gomega.HaveOccurred())
+		// 	gomega.Eventually(func() bool {
+		// 		cluster, err := clusterClient.ClusterV1().ManagedClusters().Get(context.Background(), defaultManagedCluster, metav1.GetOptions{})
+		// 		gomega.Expect(err).ToNot(gomega.HaveOccurred())
 
-				if len(cluster.Spec.ManagedClusterClientConfigs) == 0 {
-					return false
-				}
-				for _, config := range cluster.Spec.ManagedClusterClientConfigs {
-					if config.URL != apiserverAddress {
-						continue
-					}
-					if reflect.DeepEqual(config.CABundle, fakeSecret.Data["tls.crt"]) {
-						return true
-					}
-				}
-				return false
-			}, eventuallyTimeout, eventuallyInterval).Should(gomega.BeTrue())
+		// 		if len(cluster.Spec.ManagedClusterClientConfigs) == 0 {
+		// 			return false
+		// 		}
+		// 		for _, config := range cluster.Spec.ManagedClusterClientConfigs {
+		// 			if config.URL != apiserverAddress {
+		// 				continue
+		// 			}
+		// 			if reflect.DeepEqual(config.CABundle, fakeSecret.Data["tls.crt"]) {
+		// 				return true
+		// 			}
+		// 		}
+		// 		return false
+		// 	}, eventuallyTimeout, eventuallyInterval).Should(gomega.BeTrue())
 
-			//rollback apiserver and delete secret
-			newApiserver.Spec.ServingCerts.NamedCertificates = []configv1.APIServerNamedServingCert{}
-			_, err = ocpClient.ConfigV1().APIServers().Update(context.TODO(), newApiserver, metav1.UpdateOptions{})
-			gomega.Expect(err).ToNot(gomega.HaveOccurred())
-			err = kubeClient.CoreV1().Secrets(utils.OpenshiftConfigNamespace).Delete(context.TODO(), fakesecretName, metav1.DeleteOptions{})
-			gomega.Expect(err).ToNot(gomega.HaveOccurred())
-		})
+		// 	//rollback apiserver and delete secret
+		// 	newApiserver.Spec.ServingCerts.NamedCertificates = []configv1.APIServerNamedServingCert{}
+		// 	_, err = ocpClient.ConfigV1().APIServers().Update(context.TODO(), newApiserver, metav1.UpdateOptions{})
+		// 	gomega.Expect(err).ToNot(gomega.HaveOccurred())
+		// 	err = kubeClient.CoreV1().Secrets(utils.OpenshiftConfigNamespace).Delete(context.TODO(), fakesecretName, metav1.DeleteOptions{})
+		// 	gomega.Expect(err).ToNot(gomega.HaveOccurred())
+		// })
 
 		ginkgo.It("Get CA from configmap", func() {
 			//Only need to test this case in ocp
