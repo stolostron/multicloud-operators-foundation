fbfe07e08098ee3441fcf39c5271b8d0f5add6e8
xuezhaojun <zxue@redhat.com>
2024-01-04 17:21:56 +0800

    Remove customized handlers.
    Signed-off-by: xuezhaojun <zxue@redhat.com>

--- pkg/klusterlet/clusterclaim/manager.go
+++ pkg/klusterlet/clusterclaim/manager.go
@@ -1,20 +1,12 @@
 package clusterclaim
 
 import (
-	"context"
-	"fmt"
-
 	clusterv1beta1 "github.com/stolostron/cluster-lifecycle-api/clusterinfo/v1beta1"
-	"k8s.io/client-go/tools/cache"
-	"k8s.io/client-go/util/workqueue"
 	clusterv1alpha1informer "open-cluster-management.io/api/client/cluster/informers/externalversions/cluster/v1alpha1"
 	clusterv1alpha1 "open-cluster-management.io/api/cluster/v1alpha1"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
-	"sigs.k8s.io/controller-runtime/pkg/event"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
-	"sigs.k8s.io/controller-runtime/pkg/predicate"
-	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 	"sigs.k8s.io/controller-runtime/pkg/source"
 )
 
@@ -25,8 +17,7 @@ func (r *clusterClaimReconciler) SetupWithManager(mgr ctrl.Manager, clusterInfor
 		return err
 	}
 
-	claimSource := NewClusterClaimSource(clusterInformer)
-	if err := c.Watch(claimSource, NewClusterClaimEventHandler()); err != nil {
+	if err := c.Watch(source.Kind(mgr.GetCache(), &clusterv1alpha1.ClusterClaim{}), &handler.EnqueueRequestForObject{}); err != nil {
 		return err
 	}
 
@@ -38,75 +29,4 @@ func (r *clusterClaimReconciler) SetupWithManager(mgr ctrl.Manager, clusterInfor
 	}
 
 	return nil
-}
-
-// NewClusterClaimSource returns an event source for cluster claims
-func NewClusterClaimSource(clusterInformers clusterv1alpha1informer.ClusterClaimInformer) source.Source {
-	return &clusterClaimSource{
-		claimInformer: clusterInformers.Informer(),
-	}
-}
-
-// clusterClaimSource is the event source of cluster claims on managed cluster.
-type clusterClaimSource struct {
-	claimInformer cache.SharedIndexInformer
-}
-
-var _ source.SyncingSource = &clusterClaimSource{}
-
-func (s *clusterClaimSource) Start(ctx context.Context, handler handler.EventHandler, queue workqueue.RateLimitingInterface,
-	predicates ...predicate.Predicate) error {
-	// all predicates are ignored
-	s.claimInformer.AddEventHandler(cache.ResourceEventHandlerFuncs{
-		AddFunc: func(obj interface{}) {
-			handler.Create(ctx, event.CreateEvent{
-				Object: obj.(*clusterv1alpha1.ClusterClaim),
-			}, queue)
-		},
-		UpdateFunc: func(oldObj, newObj interface{}) {
-			handler.Update(ctx, event.UpdateEvent{
-				ObjectOld: oldObj.(*clusterv1alpha1.ClusterClaim),
-				ObjectNew: newObj.(*clusterv1alpha1.ClusterClaim),
-			}, queue)
-		},
-		DeleteFunc: func(obj interface{}) {
-			handler.Delete(ctx, event.DeleteEvent{
-				Object: obj.(*clusterv1alpha1.ClusterClaim),
-			}, queue)
-		},
-	})
-
-	return nil
-}
-
-func (s *clusterClaimSource) WaitForSync(ctx context.Context) error {
-	if ok := cache.WaitForCacheSync(ctx.Done(), s.claimInformer.HasSynced); !ok {
-		return fmt.Errorf("Never achieved initial sync")
-	}
-	return nil
-}
-
-// ClusterClaimEventHandler maps any event to an empty request
-func NewClusterClaimEventHandler() *clusterClaimEventHandler {
-	return &clusterClaimEventHandler{}
-}
-
-type clusterClaimEventHandler struct{}
-
-var _ handler.EventHandler = &clusterClaimEventHandler{}
-
-func (e *clusterClaimEventHandler) Create(_ context.Context, evt event.CreateEvent, q workqueue.RateLimitingInterface) {
-	q.Add(reconcile.Request{})
-}
-
-func (e *clusterClaimEventHandler) Update(_ context.Context, evt event.UpdateEvent, q workqueue.RateLimitingInterface) {
-	q.Add(reconcile.Request{})
-}
-
-func (e *clusterClaimEventHandler) Delete(_ context.Context, evt event.DeleteEvent, q workqueue.RateLimitingInterface) {
-	q.Add(reconcile.Request{})
-}
-
-func (e *clusterClaimEventHandler) Generic(_ context.Context, evt event.GenericEvent, q workqueue.RateLimitingInterface) {
-	q.Add(reconcile.Request{})
 }
--- pkg/klusterlet/clusterinfo/manager.go
+++ pkg/klusterlet/clusterinfo/manager.go
@@ -1,80 +1,19 @@
 package controllers
 
 import (
-	"context"
-	"fmt"
-
 	clusterv1beta1 "github.com/stolostron/cluster-lifecycle-api/clusterinfo/v1beta1"
-	"github.com/stolostron/multicloud-operators-foundation/pkg/klusterlet/clusterclaim"
-	"k8s.io/client-go/tools/cache"
-	"k8s.io/client-go/util/workqueue"
+	corev1 "k8s.io/api/core/v1"
 	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/event"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
-	"sigs.k8s.io/controller-runtime/pkg/predicate"
-	"sigs.k8s.io/controller-runtime/pkg/reconcile"
-	"sigs.k8s.io/controller-runtime/pkg/source"
+
+	clusterv1alpha1 "open-cluster-management.io/api/cluster/v1alpha1"
 )
 
 func (r *ClusterInfoReconciler) SetupWithManager(mgr ctrl.Manager) error {
-	claimSource := clusterclaim.NewClusterClaimSource(r.ClaimInformer)
-	nodeSource := &nodeSource{nodeInformer: r.NodeInformer.Informer()}
+	// Watch ClusterClaims and Nodes changes for ManagedClusterInfo reconcile
 	return ctrl.NewControllerManagedBy(mgr).
-		WatchesRawSource(claimSource, clusterclaim.NewClusterClaimEventHandler()).
-		WatchesRawSource(nodeSource, &nodeEventHandler{}).
+		Watches(&clusterv1alpha1.ClusterClaim{}, &handler.EnqueueRequestForObject{}).
+		Watches(&corev1.Node{}, &handler.EnqueueRequestForObject{}).
 		For(&clusterv1beta1.ManagedClusterInfo{}).
 		Complete(r)
-}
-
-// nodeSource is the event source of nodes on managed cluster.
-type nodeSource struct {
-	nodeInformer cache.SharedIndexInformer
-}
-
-var _ source.SyncingSource = &nodeSource{}
-
-func (s *nodeSource) Start(ctx context.Context, handler handler.EventHandler, queue workqueue.RateLimitingInterface,
-	predicates ...predicate.Predicate) error {
-	// all predicates are ignored
-	s.nodeInformer.AddEventHandler(cache.ResourceEventHandlerFuncs{
-		AddFunc: func(obj interface{}) {
-			handler.Create(ctx, event.CreateEvent{}, queue)
-		},
-		UpdateFunc: func(oldObj, newObj interface{}) {
-			handler.Update(ctx, event.UpdateEvent{}, queue)
-		},
-		DeleteFunc: func(obj interface{}) {
-			handler.Delete(ctx, event.DeleteEvent{}, queue)
-		},
-	})
-
-	return nil
-}
-
-func (s *nodeSource) WaitForSync(ctx context.Context) error {
-	if ok := cache.WaitForCacheSync(ctx.Done(), s.nodeInformer.HasSynced); !ok {
-		return fmt.Errorf("never achieved initial sync")
-	}
-	return nil
-}
-
-// nodeEventHandler maps any event to an empty request
-type nodeEventHandler struct{}
-
-var _ handler.EventHandler = &nodeEventHandler{}
-
-func (e *nodeEventHandler) Create(ctx context.Context, evt event.CreateEvent, q workqueue.RateLimitingInterface) {
-	q.Add(reconcile.Request{})
-}
-
-func (e *nodeEventHandler) Update(ctx context.Context, evt event.UpdateEvent, q workqueue.RateLimitingInterface) {
-	q.Add(reconcile.Request{})
-}
-
-func (e *nodeEventHandler) Delete(ctx context.Context, evt event.DeleteEvent, q workqueue.RateLimitingInterface) {
-	q.Add(reconcile.Request{})
-}
-
-func (e *nodeEventHandler) Generic(ctx context.Context, evt event.GenericEvent, q workqueue.RateLimitingInterface) {
-	q.Add(reconcile.Request{})
 }
