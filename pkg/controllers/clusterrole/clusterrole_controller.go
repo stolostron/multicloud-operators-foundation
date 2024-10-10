2f277bd9933fb20844a09ffa122775f254c89889
xuezhaojun <zxue@redhat.com>
2024-10-10 11:02:12 +0800

    Add clusterrole watch in the clusterrole-controller.
    Signed-off-by: xuezhaojun <zxue@redhat.com>

--- pkg/controllers/clusterrole/clusterrole_controller.go
+++ pkg/controllers/clusterrole/clusterrole_controller.go
@@ -2,19 +2,25 @@ package clusterrole
 
 import (
 	"context"
+	"fmt"
+	"strings"
 
 	"github.com/stolostron/multicloud-operators-foundation/pkg/utils"
+	rbacv1 "k8s.io/api/rbac/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/util/workqueue"
 	"k8s.io/klog"
 	clusterv1 "open-cluster-management.io/api/cluster/v1"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/builder"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/event"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
-	"sigs.k8s.io/controller-runtime/pkg/source"
 )
 
 const (
@@ -44,20 +50,76 @@ func newReconciler(mgr manager.Manager, kubeClient kubernetes.Interface) reconci
 	}
 }
 
+var _ handler.EventHandler = &enqueueRequestForClusterRole{}
+
+type enqueueRequestForClusterRole struct{}
+
+func (e *enqueueRequestForClusterRole) enqueue(clusterroleName string, q workqueue.TypedRateLimitingInterface[reconcile.Request]) {
+	clusterName, err := getClusterNameFromClusterRoleName(clusterroleName)
+	if err != nil {
+		klog.Info("error getting cluster name from clusterrole name", err)
+		return
+	}
+	q.Add(reconcile.Request{NamespacedName: types.NamespacedName{Name: clusterName}})
+}
+
+func (e *enqueueRequestForClusterRole) Create(ctx context.Context,
+	evt event.TypedCreateEvent[client.Object], q workqueue.TypedRateLimitingInterface[reconcile.Request]) {
+}
+
+func (e *enqueueRequestForClusterRole) Update(ctx context.Context,
+	evt event.TypedUpdateEvent[client.Object], q workqueue.TypedRateLimitingInterface[reconcile.Request]) {
+	e.enqueue(evt.ObjectNew.GetName(), q)
+}
+
+func (e *enqueueRequestForClusterRole) Delete(ctx context.Context,
+	evt event.TypedDeleteEvent[client.Object], q workqueue.TypedRateLimitingInterface[reconcile.Request]) {
+	e.enqueue(evt.Object.GetName(), q)
+}
+
+func (e *enqueueRequestForClusterRole) Generic(ctx context.Context,
+	evt event.TypedGenericEvent[client.Object], q workqueue.TypedRateLimitingInterface[reconcile.Request]) {
+}
+
 // add adds a new Controller to mgr with r as the reconcile.Reconciler
 func add(mgr manager.Manager, r reconcile.Reconciler) error {
-	// Create a new controller
-	c, err := controller.New("clusterrole-controller", mgr, controller.Options{Reconciler: r})
-	if err != nil {
-		return err
-	}
+	return ctrl.NewControllerManagedBy(mgr).Named("clusterrole-controller").Watches(
+		&clusterv1.ManagedCluster{},
+		&handler.EnqueueRequestForObject{},
+	).WatchesMetadata(
+		&rbacv1.ClusterRole{},
+		&enqueueRequestForClusterRole{},
+		builder.WithPredicates(predicate.Funcs{
+			GenericFunc: func(e event.GenericEvent) bool { return false },
+			DeleteFunc: func(e event.DeleteEvent) bool {
+				return isValiedClusterRole(e.Object.GetName())
+			},
+			CreateFunc: func(e event.CreateEvent) bool { return false },
+			UpdateFunc: func(e event.UpdateEvent) bool {
+				return isValiedClusterRole(e.ObjectNew.GetName())
+			},
+		}),
+	).Complete(r)
+}
 
-	err = c.Watch(source.Kind(mgr.GetCache(), &clusterv1.ManagedCluster{},
-		&handler.TypedEnqueueRequestForObject[*clusterv1.ManagedCluster]{}))
-	if err != nil {
-		return err
+// isValiedClusterRole tells if the clusterrole is an OCM clusterrole
+// The format of the clusterrole name is "open-cluster-management:admin:cluster-name" or "open-cluster-management:view:cluster-name"
+func isValiedClusterRole(clusterRoleName string) bool {
+	parts := strings.Split(clusterRoleName, ":")
+	if len(parts) != 3 {
+		return false
 	}
-	return nil
+	return parts[0] == "open-cluster-management" && (parts[1] == "admin" || parts[1] == "view")
+}
+
+// getClusterNameFromClusterRoleName get the cluster name from the clusterrole name
+// The format of the clusterrole name is "open-cluster-management:admin:cluster-name" or "open-cluster-management:view:cluster-name"
+func getClusterNameFromClusterRoleName(clusterRoleName string) (string, error) {
+	parts := strings.Split(clusterRoleName, ":")
+	if len(parts) != 3 {
+		return "", fmt.Errorf("invalid clusterrole name: %s", clusterRoleName)
+	}
+	return parts[2], nil
 }
 
 func (r *Reconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
