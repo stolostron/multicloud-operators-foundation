9e5e4dd20ac1b1a6a10461554e71c50b6fd9e4a6
Wei Liu <liuweixa@redhat.com>
2025-05-28 11:43:19 +0800

    support subjects for clusterpermission
    Signed-off-by: Wei Liu <liuweixa@redhat.com>

--- pkg/proxyserver/rest/project/index_test.go
+++ pkg/proxyserver/rest/project/index_test.go
@@ -3,7 +3,6 @@ package project
 import (
 	"testing"
 
-	rbacv1 "k8s.io/api/rbac/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/util/sets"
@@ -100,6 +99,42 @@ func TestIndexClusterPermissionBySubject(t *testing.T) {
 			expectError: false,
 		},
 		{
+			name: "object with both subjects and subject",
+			input: &unstructured.Unstructured{
+				Object: map[string]interface{}{
+					"metadata": map[string]interface{}{
+						"namespace": "test-ns",
+						"name":      "test-name",
+					},
+					"spec": map[string]interface{}{
+						"roleBindings": []interface{}{
+							map[string]interface{}{
+								"subject": map[string]interface{}{
+									"kind": "Group",
+									"name": "group1",
+								},
+								"subjects": []interface{}{
+									map[string]interface{}{
+										"kind": "User",
+										"name": "user1",
+									},
+									map[string]interface{}{
+										"kind": "Group",
+										"name": "group2",
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			expected: []string{
+				"test-ns/test-name/User/user1",
+				"test-ns/test-name/Group/group2",
+			},
+			expectError: false,
+		},
+		{
 			name: "object with invalid subject",
 			input: &unstructured.Unstructured{
 				Object: map[string]interface{}{
@@ -168,68 +203,6 @@ func TestIndexClusterPermissionBySubject(t *testing.T) {
 			}
 
 			if !sets.NewString(result...).Equal(sets.NewString(tt.expected...)) {
-				t.Errorf("expected %v, got %v", tt.expected, result)
-			}
-		})
-	}
-}
-
-func TestToSubject(t *testing.T) {
-	tests := []struct {
-		name        string
-		input       map[string]interface{}
-		expected    *rbacv1.Subject
-		expectError bool
-	}{
-		{
-			name: "valid subject",
-			input: map[string]interface{}{
-				"subject": map[string]interface{}{
-					"kind": "User",
-					"name": "test-user",
-				},
-			},
-			expected: &rbacv1.Subject{
-				Kind: "User",
-				Name: "test-user",
-			},
-			expectError: false,
-		},
-		{
-			name: "missing subject",
-			input: map[string]interface{}{
-				"not-subject": "value",
-			},
-			expected:    nil,
-			expectError: true,
-		},
-		{
-			name: "invalid subject type",
-			input: map[string]interface{}{
-				"subject": "not-a-map",
-			},
-			expected:    nil,
-			expectError: true,
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			result, err := toSubject(tt.input)
-
-			if tt.expectError {
-				if err == nil {
-					t.Errorf("expected error, but got none")
-				}
-				return
-			}
-
-			if err != nil {
-				t.Errorf("unexpected error: %v", err)
-				return
-			}
-
-			if result.Kind != tt.expected.Kind || result.Name != tt.expected.Name {
 				t.Errorf("expected %v, got %v", tt.expected, result)
 			}
 		})
